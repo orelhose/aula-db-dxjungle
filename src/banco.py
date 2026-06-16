bf957c3b92d6a22d58ef34709d708f704150b286
Juliana Oliveira Costa <2024324699@ifam.edu.br>
2026-06-16 08:40:41 -0400

    terceira commit

--- src/banco.py
+++ src/banco.py
@@ -1,1 +1,30 @@
-print("Hello World")
+import sqlite3
+from pathlib import Path
+
+CAMINHO_BANCO = Path('dados/aula-01.db')
+CAMINHO_BANCO.parent.mkdir(exist_ok=True)
+
+def conectar():
+    conexao = sqlite3.connect(CAMINHO_BANCO)
+    conexao.execute('PRAGMA foreign_keys = ON')
+    return conexao
+
+with conectar() as conn:
+    conn.execute('''
+        CREATE TABLE IF NOT EXISTS turmas (
+                id_turma INTEGER PRIMARY KEY AUTOINCREMENT,
+                nome TEXT NOT NULL,
+                curso TEXT NOT NULL,
+                ano INTEGER NOT NULL
+                );
+    ''');
+    conn.commit()
+ 
+with conectar() as conn:
+    conn.execute('''
+        INSERT INTO turmas (nome, curso, ano) 
+        VALUES('2 INFO', 'Informática', 2)
+    ''');  
+    conn.commit()
+
+conn.close()
